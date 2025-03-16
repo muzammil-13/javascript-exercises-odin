b9febac598cee5eb90e49d20b99e5bb6949106e3
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-03-16 08:32:12 +0530

    Implement reverseString function and enable tests for various cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    string=string.split('').reverse().join('')
+    return string;
 };
 
 // Do not edit below this line
