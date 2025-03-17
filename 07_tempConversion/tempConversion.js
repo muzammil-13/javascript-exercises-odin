2a7dcd3f7772837e8abd5e7fc1511c815bcfa3e0
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-03-17 20:22:52 +0530

    Refactor temperature conversion functions to accept parameters and return rounded results; update tests to enable all cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  temperature=(temperature-32)*5/9;
+  // temperature=Math.round(temperature);
+  temperature=parseFloat(temperature.toFixed(1))
+  return temperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  temperature=(temperature*(9/5)+32);
+  // temperature=Math.round(temperature);
+  temperature=parseFloat(temperature.toFixed(1));
+  return temperature;
 };
 
 // Do not edit below this line
