7051b4ade1975c7c59f3176b27e4867c7cb1e5de
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-03-17 19:39:04 +0530

    Implement sumAll function with input validation and update tests to cover various scenarios

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    if(typeof n1!=='number' || typeof n2!=='number' || !Number.isInteger(n1) || !Number.isInteger(n2) || n1<0||n2<0){
+        return 'ERROR'
+    }
+    
+    if(n1>n2){
+        [n1,n2]=[n2,n1]
+    }
+    let sum=0;
+    for(let i=n1;i<=n2;i++){
+        sum+=i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
